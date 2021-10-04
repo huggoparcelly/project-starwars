7202b543920db38c17e0bf420b4fd5468a7dfff8
Huggo Parcelly <h.parcelly@gmail.com>
2021-10-04 18:32:45 -0300

    Updates

---
+++ precache-manifest.e6475b8e3f369868d3377433e4ed8795.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cb5ae96ddc36d1f6d9d0238647409f3",
+    "url": "/project-starwars/index.html"
+  },
+  {
+    "revision": "ad04bd30e6a5da5a8687",
+    "url": "/project-starwars/static/css/main.0c8dd27f.chunk.css"
+  },
+  {
+    "revision": "0c3007c3e080edfb0252",
+    "url": "/project-starwars/static/js/2.6829e922.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/project-starwars/static/js/2.6829e922.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad04bd30e6a5da5a8687",
+    "url": "/project-starwars/static/js/main.fdf39bfc.chunk.js"
+  },
+  {
+    "revision": "50d13460a50225da1f48",
+    "url": "/project-starwars/static/js/runtime-main.0b79d0d8.js"
+  }
+]);
